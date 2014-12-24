a19bf0b5ac037425b73611c88a2ebb5799d42e35
Rumato Estorsky <rumatoest@yandex.ru>
2014-12-24 16:32:40 +0300

    Initial commit

---
+++ src/main/java/com/googlecode/spring4jsf/scope/ViewScope.java
@@ -0,0 +1,132 @@
+package com.googlecode.spring4jsf.scope;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.config.Scope;
+
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PreDestroyViewMapEvent;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionBindingListener;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.WeakHashMap;
+
+/**
+ * @author m.nikolaev Date: 21.11.12 Time: 0:37
+ */
+public class ViewScope implements Scope, Serializable, HttpSessionBindingListener {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final Logger logger = LoggerFactory.getLogger(ViewScope.class);
+
+    private final WeakHashMap<HttpSession, Set<ViewScopeViewMapListener>> sessionToListeners = new WeakHashMap<>();
+
+    @Override
+    public Object get(String name, ObjectFactory objectFactory) {
+        Map<String, Object> viewMap = FacesContext.getCurrentInstance().getViewRoot().getViewMap();
+        //noinspection SynchronizationOnLocalVariableOrMethodParameter
+        synchronized (viewMap) {
+            if (viewMap.containsKey(name)) {
+                return viewMap.get(name);
+            } else {
+                logger.debug("Creating bean {}", name);
+                Object object = objectFactory.getObject();
+                viewMap.put(name, object);
+
+                return object;
+            }
+        }
+    }
+
+    @Override
+    public Object remove(String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getConversationId() {
+        return null;
+    }
+
+    @Override
+    public void registerDestructionCallback(String name, Runnable callback) {
+        logger.debug("registerDestructionCallback for bean {}", name);
+        UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
+        ViewScopeViewMapListener listener
+            = new ViewScopeViewMapListener(viewRoot, name, callback, this);
+
+        viewRoot.subscribeToViewEvent(PreDestroyViewMapEvent.class, listener);
+
+        HttpSession httpSession = (HttpSession)FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+        final Set<ViewScopeViewMapListener> sessionListeners;
+        synchronized (sessionToListeners) {
+            if (!sessionToListeners.containsKey(httpSession)) {
+                sessionToListeners.put(httpSession, new HashSet<ViewScopeViewMapListener>());
+            }
+            sessionListeners = sessionToListeners.get(httpSession);
+        }
+        //noinspection SynchronizationOnLocalVariableOrMethodParameter
+        synchronized (sessionListeners) {
+            Set<ViewScopeViewMapListener> toRemove = new HashSet<>();
+            for (ViewScopeViewMapListener viewMapListener : sessionListeners) {
+                if (viewMapListener.checkRoot()) {
+                    toRemove.add(viewMapListener);
+                }
+            }
+            sessionListeners.removeAll(toRemove);
+            sessionListeners.add(listener);
+        }
+        if (!FacesContext.getCurrentInstance().getExternalContext().getSessionMap().containsKey("sessionBindingListener")) {
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("sessionBindingListener", this);
+        }
+
+    }
+
+    @Override
+    public Object resolveContextualObject(String key) {
+        return null;
+    }
+
+    @Override
+    public void valueBound(HttpSessionBindingEvent event) {
+        logger.debug("Session event bound {}", event.getName());
+    }
+
+    @Override
+    public void valueUnbound(HttpSessionBindingEvent event) {
+        logger.debug("Session event unbound {}", event.getName());
+        final Set<ViewScopeViewMapListener> listeners;
+        synchronized (sessionToListeners) {
+            if (sessionToListeners.containsKey(event.getSession())) {
+                listeners = sessionToListeners.get(event.getSession());
+                sessionToListeners.remove(event.getSession());
+            } else {
+                listeners = null;
+            }
+        }
+        if (listeners != null) {
+            for (ViewScopeViewMapListener listener : listeners) {
+                listener.doCallback();
+            }
+        }
+    }
+
+    public void clearFromListener(ViewScopeViewMapListener listener) {
+        logger.debug("Removing listener from map");
+        HttpSession httpSession = (HttpSession)FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+        if (httpSession != null) {
+            synchronized (sessionToListeners) {
+                if (sessionToListeners.containsKey(httpSession)) {
+                    sessionToListeners.get(httpSession).remove(listener);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/googlecode/spring4jsf/scope/ViewScopeViewMapListener.java
@@ -0,0 +1,69 @@
+package com.googlecode.spring4jsf.scope;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.faces.component.UIViewRoot;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.PreDestroyViewMapEvent;
+import javax.faces.event.SystemEvent;
+import javax.faces.event.ViewMapListener;
+import java.lang.ref.WeakReference;
+
+/**
+ * @author Michail Nikolaev ate: 21.11.12 Time: 0:37
+ */
+public class ViewScopeViewMapListener implements ViewMapListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(ViewScopeViewMapListener.class);
+
+    private String name;
+
+    private Runnable callback;
+
+    private boolean callbackCalled = false;
+
+    private WeakReference<UIViewRoot> uiViewRootWeakReference;
+
+    private ViewScope viewScope;
+
+    public ViewScopeViewMapListener(UIViewRoot root, String name, Runnable callback, ViewScope viewScope) {
+        this.name = name;
+        this.callback = callback;
+        this.uiViewRootWeakReference = new WeakReference<>(root);
+        this.viewScope = viewScope;
+    }
+
+    @Override
+    public void processEvent(SystemEvent event) throws AbortProcessingException {
+        if (event instanceof PreDestroyViewMapEvent) {
+            logger.debug("Going call callback for bean {}", name);
+            doCallback();
+            viewScope.clearFromListener(this);
+        }
+    }
+
+    public boolean checkRoot() {
+        if (uiViewRootWeakReference.get() == null) {
+            doCallback();
+            return true;
+        }
+        return false;
+    }
+
+    public synchronized void doCallback() {
+        logger.debug("Going call callback for bean {}", name);
+        if (!callbackCalled) {
+            try {
+                callback.run();
+            } finally {
+                callbackCalled = true;
+            }
+        }
+    }
+
+    @Override
+    public boolean isListenerForSource(Object source) {
+        return (source == uiViewRootWeakReference.get());
+    }
+}
